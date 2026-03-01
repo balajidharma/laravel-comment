5ad93d68097e790bced202968ff6caf3613dac82
balajidharma <balajidharma@hotmail.com>
2026-03-01 11:45:40 -0500

    feat: add comment() and commentAsUser() helper methods to HasComments trait

--- src/Traits/HasComments.php
+++ src/Traits/HasComments.php
@@ -2,6 +2,7 @@
 
 namespace BalajiDharma\LaravelComment\Traits;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Support\Facades\Config;
 
@@ -34,5 +35,40 @@ trait HasComments
     public function rejectedComments()
     {
         return $this->morphMany(Config::get('comment.models.comment'), 'commentable')->where('status', Config::get('comment.status.rejected'));
+    }
+
+    /**
+     * Add a comment as the currently authenticated user.
+     *
+     * @param  string   $content
+     * @param  int|null $parentId
+     * @return Model
+     */
+    public function comment(string $content, ?int $parentId = null): Model
+    {
+        return $this->commentAsUser(auth()->user(), $content, $parentId);
+    }
+
+    /**
+     * Add a comment as a specific user.
+     *
+     * @param  Model    $user
+     * @param  string   $content
+     * @param  int|null $parentId
+     * @return Model
+     */
+    public function commentAsUser(Model $user, string $content, ?int $parentId = null): Model
+    {
+        $commentClass = Config::get('comment.models.comment');
+
+        return $commentClass::create([
+            'content'          => $content,
+            'commenter_type'   => $user->getMorphClass(),
+            'commenter_id'     => $user->getKey(),
+            'commentable_type' => $this->getMorphClass(),
+            'commentable_id'   => $this->getKey(),
+            'parent_id'        => $parentId,
+            'status'           => Config::get('comment.default_status', 1),
+        ]);
     }
 }
